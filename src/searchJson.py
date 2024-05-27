e760d4c27412f720e915d3f3d819b91e26e4c363
Cerrito Paolo <paolo.cerrito@uniroma1.it>
2024-05-27 15:54:27 +0200

    Argument parsewr and optionts to use command line parameters

--- src/searchJson.py
+++ src/searchJson.py
@@ -2,26 +2,39 @@ import json
 import pandas as pd
 import re
 import sys, getopt
+import argparse
 from pprint import pprint
 
 
-def main(argv):
-    with open('../example/people-2024-04-29.json', 'r') as f:
-        data = f.read()
-    
-    #loading data into json dict
-    dpm_dict = json.loads(data)
-    
-    users=dpm_dict['rows']
-    
-    #convert dict to dataframe
-    df = pd.DataFrame(users)
-    
-    #filter by column mail not equalt to
-    #results=df.loc[df['mail'] != '*@uniroma1.it']
-    
-    #try lamba function for filter
-    
-    results = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
-    
-    results.to_excel(excel_writer='../results/export.xlsx', sheet_name='Nouniroma1')
+input_file=''
+output_file=''
+parser = argparse.ArgumentParser(description="Argument parser for command line options")
+parser.add_argument('-i','--input', nargs = '?', help = 'Input file to read values in json format', dest="inputFile", required = True)
+parser.add_argument('-o', '--output', nargs = '?', default = sys.stdout, help = 'Output file to write to, defualt standard output', dest = "outputFile")
+args = parser.parse_args()
+
+input_file=args.inputFile
+output_file=args.outputFile
+
+with open(input_file, 'r') as f:
+    data = f.read()
+
+#loading data into json dict
+dpm_dict = json.loads(data)
+
+users=dpm_dict['rows']
+
+#convert dict to dataframe
+df = pd.DataFrame(users)
+
+#filter by column mail not equalt to
+#results=df.loc[df['mail'] != '*@uniroma1.it']
+
+#try lamba function for filter
+
+results = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
+
+if output_file == sys.stdout:
+    print(results)
+else:
+    results.to_excel(excel_writer=output_file, sheet_name='Nouniroma1')
