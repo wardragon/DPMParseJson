44a62bc550dbbdafb2142962430667c2cc2edebb
Cerrito Paolo <paolo.cerrito@uniroma1.it>
2024-05-27 15:19:35 +0200

    Defined main function

--- src/searchJson.py
+++ src/searchJson.py
@@ -1,25 +1,27 @@
 import json
 import pandas as pd
 import re
+import sys, getopt
 from pprint import pprint
 
 
-with open('../example/people-2024-04-29.json', 'r') as f:
-    data = f.read()
-
-#loading data into json dict
-dpm_dict = json.loads(data)
-
-users=dpm_dict['rows']
-
-#convert dict to dataframe
-df = pd.DataFrame(users)
-
-#filter by column mail not equalt to
-#results=df.loc[df['mail'] != '*@uniroma1.it']
-
-#try lamba function for filter
-
-results = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
-
-results.to_excel(excel_writer='../results/export.xlsx', sheet_name='Nouniroma1')
+def main(argv):
+    with open('../example/people-2024-04-29.json', 'r') as f:
+        data = f.read()
+    
+    #loading data into json dict
+    dpm_dict = json.loads(data)
+    
+    users=dpm_dict['rows']
+    
+    #convert dict to dataframe
+    df = pd.DataFrame(users)
+    
+    #filter by column mail not equalt to
+    #results=df.loc[df['mail'] != '*@uniroma1.it']
+    
+    #try lamba function for filter
+    
+    results = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
+    
+    results.to_excel(excel_writer='../results/export.xlsx', sheet_name='Nouniroma1')
