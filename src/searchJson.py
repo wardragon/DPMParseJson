8df0a217986675997a06a8d955447a4688701083
Cerrito Paolo <paolo.cerrito@uniroma1.it>
2024-07-08 15:08:56 +0200

    aggiunti paramtri di write per i file con caselle uniroma1, aggiunto filtro per scrivere i non assegnati su nouniroma1, scritto codice per scrivere il json degli uniroma1 da inviare a Studio Storti

--- src/searchJson.py
+++ src/searchJson.py
@@ -4,17 +4,21 @@ import re
 import sys, getopt
 import argparse
 from pprint import pprint
-
+from collections import OrderedDict, defaultdict
 
 input_file=''
 output_file=''
 parser = argparse.ArgumentParser(description="Argument parser for command line options")
 parser.add_argument('-i','--input', nargs = '?', help = 'Input file to read values in json format', dest="inputFile", required = True)
-parser.add_argument('-o', '--output', nargs = '?', default = sys.stdout, help = 'Output file to write to, defualt standard output', dest = "outputFile")
+parser.add_argument('-ono', '--outputno', nargs = '?', default = sys.stdout, help = 'Output file xlsx to write nouniroma1 to, default standard output', dest = "outputFileNo")
+parser.add_argument('-o', '--output', nargs = '?', default = sys.stdout, help = 'Output file xlsx to write uniroma1 to, default standard output', dest = "outputFile")
+parser.add_argument('-ojson', '--outputjson', nargs = '?', default = sys.stdout, help = 'Output file json to write uniroma1 to, default none', dest = "outputFileJson")
 args = parser.parse_args()
 
 input_file=args.inputFile
-output_file=args.outputFile
+output_file_nouni=args.outputFileNo
+output_file_uni=args.outputFile
+json_file_out=args.outputFileJson
 
 with open(input_file, 'r') as f:
     data = f.read()
@@ -32,9 +36,24 @@ df = pd.DataFrame(users)
 
 #try lamba function for filter
 
-results = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
+nouniroma1 = df.apply(lambda row: row[~df['mail'].str.contains("@*uniroma1.it")])
+nostruct =  df.apply(lambda row: row[df['structure_name'].str.contains("NON ASSEGNATO")])
+atuniroma1 = df.apply(lambda row: row[df['mail'].str.contains("@*uniroma1.it")])
+uniroma1= atuniroma1.apply(lambda row: row[~atuniroma1['structure_name'].str.contains("NON ASSEGNATO")])
 
-if output_file == sys.stdout:
-    print(results)
+nouniroma1=nouniroma1.append(nostruct)
+
+if output_file_nouni == sys.stdout or output_file_uni==sys.stdout:
+    print("--- Elenco No Uniroma1 ----------------")
+    pprint(nouniroma1)
+    print("---------------------------------------")
+    print("--- Elenco Uniroma1 ----------------")
+    pprint(uniroma1)
+    print("---------------------------------------")
 else:
-    results.to_excel(excel_writer=output_file, sheet_name='Nouniroma1')
+    nouniroma1.to_excel(excel_writer=output_file_nouni, sheet_name='No_uniroma1_email')
+    uniroma1.to_excel(excel_writer=output_file_uni, sheet_name='Uniroma1_email')
+
+    if json_file_out != sys.stdout:
+     with open(json_file_out, 'w') as json_file:
+            json_file.write(json.dumps(uniroma1.to_dict('records')))
